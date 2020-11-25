d7865207ef4197874fd5be637bc225e975d56b80
Esteve Martin <esteve.martin@mpasolutions.es>
2020-11-25 11:17:15 +0100

    Code cleaned

--- routes/auth.js
+++ routes/auth.js
@@ -14,56 +14,47 @@ const {
 } = require("../helpers/middlewares");
 
 
-//  POST '/signup'
+// POST '/signup'
 router.post("/signup", isNotLoggedIn(), validationLoggin(), async (req, res, next) => {
     const {username, password, email} = req.body;
-    console.log("INSIDE SIGNUP POST")
-    console.log(username,password,email)
+    // console.log("INSIDE SIGNUP POST")
+    // console.log(username,password,email)
     // let username2="@"+username
     try {
-        const usernameExists = await User.findOne({ username:username }, "username")
-        if (usernameExists) return next(createError(400));
-
+      const usernameExists = await User.findOne({ username:username }, "username")
+      if (usernameExists) return next(createError(400));
     else {
-       console.log("can create new user: ", username)
-
-        const salt = bcrypt.genSaltSync(saltRounds);
-        const hashPass = bcrypt.hashSync(password, salt);
-        const newUser = await User.create({username:username, password:hashPass, email:email})
-        req.session.currentUser = newUser;
-        console.log(newUser)
-        res.status(200).json(newUser);
+      //  console.log("can create new user: ", username)
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashPass = bcrypt.hashSync(password, salt);
+      const newUser = await User.create({username:username, password:hashPass, email:email})
+      req.session.currentUser = newUser;
+      // console.log(newUser)
+      res.status(200).json(newUser);
         }  
     } catch (error) {
         next(error);
     }
-}
-);
+});
 
-
-//  POST '/login'
+// POST '/login'
 router.post("/login", isNotLoggedIn(), validationLoggin(), async (req, res, next) => {
-    const { username, password } = req.body;
-    try {
-        const user = await User.findOne({ username});
-        if (!user) {
-            next(createError(404));
-          } else if (bcrypt.compareSync(password, user.password)) {
-            req.session.currentUser = user;
-            res.status(200).json(user);
-            return;
-          } else {
-            next(createError(401));
-          }
-        } catch (error) {
-          next(error);
-        }
-      }
-    );
-
-
-
-
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username});
+    if (!user) {
+        next(createError(404));
+    } else if (bcrypt.compareSync(password, user.password)) {
+      req.session.currentUser = user;
+      res.status(200).json(user);
+      return;
+    } else {
+      next(createError(401));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 // POST '/logout'
 router.post("/logout", isLoggedIn(), (req, res, next) => {
@@ -72,17 +63,15 @@ router.post("/logout", isLoggedIn(), (req, res, next) => {
     return;
 });
 
+// GET  '/me'
+router.get("/me", isLoggedIn(), (req, res, next) => {
+  req.session.currentUser.password = "*";
+  res.json(req.session.currentUser);
+});
 
-// GET '/private'
+// GET  '/private'
 // router.get("/private", isLoggedIn(), (req, res, next) => {
 //     res.status(200).json({ message: "Test - User is logged in" });
 // });
-
-
-// GET '/me'
-router.get("/me", isLoggedIn(), (req, res, next) => {
-    req.session.currentUser.password = "*";
-    res.json(req.session.currentUser);
-  });
 
 module.exports = router;
--- routes/routes.js
+++ routes/routes.js
@@ -4,131 +4,64 @@ const Recipes = require("../models/recipes");
 const User = require("../models/user");
 
 
-
+// RECIPES
 router.get('/recipes', async (req, res, next) => {
     try {
         const getRecipes = await Recipes.find().populate('author')
         res.status(200).json(getRecipes)
     } catch (error) {
         console.log('/recipes (GET) ERROR: ', error)
+        res.status(400).json(error)
     }
 })
-
+router.get('/recipes/:id', async (req,res,next)=>{
+    try {
+        // console.log("EN SERVER, Req.params.id:",req.params.id)
+        const recipeId =req.params.id
+        const getRecipe = await Recipes.findOne({_id:recipeId}).populate('author')
+        // console.log("RECIPIE RESPONSE:",getRecipe)
+        res.status(200).json(getRecipe)
+    } catch (error) {
+        console.log('/recipes/:id (GET) ERROR: ', error)
+        res.status(400).json(error)
+    }
+})
 router.post('/recipes/create', async (req, res, next) => {
     try {
         const newRecipe = await Recipes.create(req.body)
         res.status(200).json(newRecipe);
     } catch(error) { 
-        console.log('/recipes/create(POST) ERROR: ', error)
+        console.log('/recipes/create (POST) ERROR: ', error)
         res.status(400).json(error)
     }
 })
-
 router.post('/recipes/update', async (req, res, next) => {
-    console.log("INSIDE RECIPES/UPDATE")
+    // console.log("INSIDE RECIPES/UPDATE")
     try {
-        console.log("Req.Body._id en Server: ",req.body._id)
+        // console.log("Req.Body._id en Server: ",req.body._id)
         // const newRecipe = await Recipes.create(req.body)
         const updatedRecipe = await Recipes.findByIdAndUpdate({_id:req.body._id},req.body)
         res.status(200).json(updatedRecipe);
     } catch(error) { 
-        console.log('/recipes/create(POST) ERROR: ', error)
+        console.log('/recipes/update (POST) ERROR: ', error)
         res.status(400).json(error)
     }
 })
-
-router.get('/recipes/:id', async (req,res,next)=>{
-    try {
-        // console.log("EN SERVER, Req.params.id:",req.params.id)
-        const recipeId =req.params.id
-
-        const getRecipe = await Recipes.findOne({_id:recipeId}).populate('author')
-        // console.log("RECIPIE RESPONSE:",getRecipe)
-        res.status(200).json(getRecipe)
-    } catch (error) {
-        console.log('/recipes (GET) ERROR: ', error)
-    }
-})
-
-router.post('/myRecipes', async (req,res,next)=>{
-    try {
-        // console.log("EN SERVER, Req.params.id:",req.params.id)
-        console.log(req.body.id)
-
-        const myId =req.body.id
-        const getRecipes = await Recipes.find({author:myId}).populate('author')
-        console.log("RECIPIE RESPONSE:",getRecipes)
-        res.status(200).json(getRecipes)
-    } catch (error) {
-        console.log('/recipes (GET) ERROR: ', error)
-    }
-})
-// router.delete('/profile/recipe/:id', (req,res,next) =>{
-//     try {
-//         Recipes.findByIdAndDelete(req.params.id)
-//         res.json({message: `Recipe is removed successfully.`})
-//     } catch (error) {
-//         res.json(error);
-//     }
-// })
-router.get('/category/:category', async (req,res,next)=>{
-    try {
-        // console.log("EN SERVER, req.params.category:",req.params.category)
-        const category =req.params.category
-        // console.log("Filtered Category:", category)
-        const getRecipe = await Recipes.find({category:category}).populate('author')
-        // console.log("RECIPIE RESPONSE:",getRecipe)
-        res.status(200).json(getRecipe)
-    } catch (error) {
-        console.log('/recipes (GET) ERROR: ', error)
-    }
-})
-
-router.get('/difficulty/:difficulty', async (req,res,next)=>{
-    try {
-        const difficulty =req.params.difficulty
-        // console.log(req.params)
-        // console.log(difficulty)
-        const getRecipe = await Recipes.find({difficulty:difficulty}).populate('author')
-        // console.log(getRecipe)
-        res.status(200).json(getRecipe)
-    } catch (error) {
-        console.log('/recipes (GET) ERROR: ', error)
-    }
-})
-
 router.post('/recipes/saved', async (req,res,next)=>{
     try {
         const userId =req.body.userId
         // console.log(req.params)
         // console.log(difficulty)
         const getRecipe = await User.find({_id:userId},'saved').populate('saved')
-        console.log(getRecipe)
+        // console.log(getRecipe)
         res.status(200).json(getRecipe)
     } catch (error) {
-        console.log('/recipes (GET) ERROR: ', error)
+        console.log('/recipes/saved (POST) ERROR: ', error)
+        res.status(400).json(error)
     }
 })
 
-router.post('/profile/update', async (req,res,next)=>{
-    try {
-        console.log(req.body)
-        // console.log(req.body.user.id)
-        // console.log(req.body.id)
-        const myId =req.body.id
-        
-        const getUser = await User.findByIdAndUpdate({_id:req.body._id},req.body)
-
-        // // console.log(req.params)
-        // // console.log(difficulty)
-        // const getRecipe = await Recipes.find({difficulty:difficulty}).populate('author')
-        console.log(getUser)
-        res.status(200).json(getUser)
-    } catch (error) {
-        console.log('/recipes (GET) ERROR: ', error)
-    }
-})
-
+//PROFILE
 router.post('/profile', async (req,res,next)=>{
     try {
         // console.log(req.body)
@@ -144,13 +77,76 @@ router.post('/profile', async (req,res,next)=>{
         // console.log(getUser)
         res.status(200).json(getUser)
     } catch (error) {
-        console.log('/recipes (GET) ERROR: ', error)
+        console.log('/profile (POST) ERROR: ', error)
+        res.status(400).json(error)
+    }
+})
+router.post('/profile/update', async (req,res,next)=>{
+    try {
+        // console.log(req.body)
+        // console.log(req.body.user.id)
+        // console.log(req.body.id)
+        const myId =req.body.id
+        
+        const getUser = await User.findByIdAndUpdate({_id:req.body._id},req.body)
+
+        // // console.log(req.params)
+        // // console.log(difficulty)
+        // const getRecipe = await Recipes.find({difficulty:difficulty}).populate('author')
+        // console.log(getUser)
+        res.status(200).json(getUser)
+    } catch (error) {
+        console.log('/profile/update (POST) ERROR: ', error)
+        res.status(400).json(error)
+    }
+})
+router.post('/myRecipes', async (req,res,next)=>{
+    try {
+        // console.log("EN SERVER, Req.params.id:",req.params.id)
+        // console.log(req.body.id)
+
+        const myId =req.body.id
+        const getRecipes = await Recipes.find({author:myId}).populate('author')
+        // console.log("RECIPIE RESPONSE:",getRecipes)
+        res.status(200).json(getRecipes)
+    } catch (error) {
+        console.log('/myRecipes (POST) ERROR: ', error)
+        res.status(400).json(error)
     }
 })
 
+// SEARCH
+router.get('/category/:category', async (req,res,next)=>{
+    try {
+        // console.log("EN SERVER, req.params.category:",req.params.category)
+        const category =req.params.category
+        // console.log("Filtered Category:", category)
+        const getRecipe = await Recipes.find({category:category}).populate('author')
+        // console.log("RECIPIE RESPONSE:",getRecipe)
+        res.status(200).json(getRecipe)
+    } catch (error) {
+        console.log('/category/:category (GET) ERROR: ', error)
+        res.status(400).json(error)
+    }
+})
+router.get('/difficulty/:difficulty', async (req,res,next)=>{
+    try {
+        const difficulty =req.params.difficulty
+        // console.log(req.params)
+        // console.log(difficulty)
+        const getRecipe = await Recipes.find({difficulty:difficulty}).populate('author')
+        // console.log(getRecipe)
+        res.status(200).json(getRecipe)
+    } catch (error) {
+        console.log('/difficulty/:difficulty (GET) ERROR: ', error)
+        res.status(400).json(error)
+    }
+})
+
+// FUNCTIONALITY
 router.post('/like', async (req,res,next)=>{
     try {
-        console.log("---------  LIKE ROUTINE    ---------")
+        // console.log("---------  LIKE ROUTINE    ---------")
         // console.log(req.body)
         const {userId,recipeId} = req.body
         
@@ -162,7 +158,7 @@ router.post('/like', async (req,res,next)=>{
         // console.log("BOOLEAN IF LIKE IS INCLUDED:",currentUser.liked.includes(recipeId))
         if (currentUser.liked.includes(recipeId)){
 
-            console.log("Recipe Likes:",currentRecipe.popularity, "Do I Like That Already? ", currentUser.liked.includes(recipeId), " --> LETS DISLIKE")
+            // console.log("Recipe Likes:",currentRecipe.popularity, "Do I Like That Already? ", currentUser.liked.includes(recipeId), " --> LETS DISLIKE")
 
             let index1 = currentUser.liked.indexOf(recipeId)
             let index2 = currentRecipe.liked.indexOf(userId)
@@ -172,18 +168,18 @@ router.post('/like', async (req,res,next)=>{
 
             currentRecipe.popularity = currentRecipe.popularity - 1
 
-            console.log("Recipe Likes:",currentRecipe.popularity, "Do I Like That Already? ", currentUser.liked.includes(recipeId))
+            // console.log("Recipe Likes:",currentRecipe.popularity, "Do I Like That Already? ", currentUser.liked.includes(recipeId))
 
         } else {
 
-            console.log("Recipe Likes:",currentRecipe.popularity, "Do I Like That Already? ", currentUser.liked.includes(recipeId), " --> LETS LIKE")
+            // console.log("Recipe Likes:",currentRecipe.popularity, "Do I Like That Already? ", currentUser.liked.includes(recipeId), " --> LETS LIKE")
 
             currentUser.liked.push(recipeId)
             currentRecipe.liked.push(userId)
 
             currentRecipe.popularity = currentRecipe.popularity + 1
 
-            console.log("Recipe Likes:",currentRecipe.popularity, "Do I Like That Already? ", currentUser.liked.includes(recipeId))
+            // console.log("Recipe Likes:",currentRecipe.popularity, "Do I Like That Already? ", currentUser.liked.includes(recipeId))
 
         }
         // console.log(currentRecipe)
@@ -193,14 +189,13 @@ router.post('/like', async (req,res,next)=>{
 
         res.status(200).json({savedUser:currentUser,savedRecipe:currentRecipe})
     } catch (error) {
-        console.log('/recipes (GET) ERROR: ', error)
+        console.log('/like (POST) ERROR: ', error)
+        res.status(400).json(error)
     }
 })
-
-
 router.post('/save', async (req,res,next)=>{
     try {
-        console.log("---------  SAVE ROUTINE    ---------")
+        // console.log("---------  SAVE ROUTINE    ---------")
         // console.log(req.body)
         const {userId,recipeId} = req.body
         
@@ -233,18 +228,23 @@ router.post('/save', async (req,res,next)=>{
         // console.log(currentRecipe)
         const savedUser = await User.findByIdAndUpdate({_id:userId},currentUser)
         // const savedRecipe = await Recipes.findByIdAndUpdate({_id:recipeId},currentRecipe)
-        console.log(currentUser)
+        // console.log(currentUser)
 
         res.status(200).json({currentUser})
     } catch (error) {
-        console.log('/recipes (GET) ERROR: ', error)
+        console.log('/save (POST) ERROR: ', error)
+        res.status(400).json(error)
     }
 })
 
 
-
-
-
-
+// router.delete('/profile/recipe/:id', (req,res,next) =>{
+//     try {
+//         Recipes.findByIdAndDelete(req.params.id)
+//         res.json({message: `Recipe is removed successfully.`})
+//     } catch (error) {
+//         res.json(error);
+//     }
+// })
 
 module.exports = router;
